dcc687f069220673d82df009df7d7457a19d4185
CASY82 <repaid11010@gmail.com>
2024-08-12 00:45:04 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## Apache Superset 연동 연습 Repository
+
+OSS 대회 준비용  
+- thymeleaf  
+- Apache Superset Docker 연동  
---
+++ src/test/java/com/study/superset/SupersetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.superset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupersetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
